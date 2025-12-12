66a3931efc5a652e43ab34153e782c4e6c57b100
TMatis <timonmatis@gmail.com>
2025-12-12 18:18:33 -0500

    fixed carousel data

--- src/components/carouselData.js
+++ src/components/carouselData.js
@@ -1,24 +1,24 @@
 const homeSlides = [
   {
-    image: "/images/GameNight.jpg",
+    image: `${process.env.PUBLIC_URL}/images/GameNight.jpg`,
     title: "Monthly YSA Meeting",
     description: "Join us every 3rd Saturday from 7-9PM for games, food, and fun!",
   },
   {
-    image: "/images/StudyGroup.jpg",
+    image: `${process.env.PUBLIC_URL}/images/StudyGroup.jpg`,
     title: "Temple Trip",
     description: "Every Wednesday at 8:30 PM, online via Zoom. meeting ID: 712 5869 3385, passcode: 3lbYsb",
   },
   {
-    image: "/images/TempleDay.jpg",
+    image: `${process.env.PUBLIC_URL}/images/TempleDay.jpg`,
     title: "Service Project",
     description: "Baptisms 1-3PM, Endowment 2-2:30PM.",
   },
   {
-    image: "/images/Caroling.png",
+    image: `${process.env.PUBLIC_URL}/images/Caroling.png`,
     title: "Monthly YSA Meeting",
     description: "Taken over for the 20th of Dec activity. Carpool to local nursing homes to sing Christmas carols.",
   }
-]
+];
 
 export default homeSlides;
